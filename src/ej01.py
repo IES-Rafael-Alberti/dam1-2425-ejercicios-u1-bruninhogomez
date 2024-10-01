12a406c8ac2d39154501e19e38e65789a30dcfbe
bruninhogomez <gcbruno@iesrafaelalberti.es>
2024-10-01 21:29:46 +0200

    commit 1

--- src/ej01.py
+++ src/ej01.py
@@ -1,5 +1,5 @@
 """este es el ejercicio 1"""
-# Ejercicio 1.2.1¶
+# Ejercicio 1.2.1
 # Escribe un programa que pida el nombre del usuario para luego darle la bienvenida.
 
 nombre = input("Introduzca un nombre:" )
---
+++ src/ej02.py
@@ -0,0 +1,10 @@
+"""este es el ejercicio 2"""
+# Ejercicio 1.2.2
+# Escribe un programa para pedirle al usuario las horas de trabajo y el precio por hora y calcule el importe total del servicio.
+
+horas =int(input("¿Cuantas horas trabajas?\n"))
+Coste =int( input("¿Cuanto cobras por horas?\n"))
+Importe = horas*Coste
+print = ("horas de trabajo: "+ str(horas))
+print = ("coste de horas: "+ str(Coste))
+print = ("Importe total: "+ str(Importe))
