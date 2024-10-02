b695f783e790b4685f42d2b89329288f30a53598
bruninhogomez <gcbruno@iesrafaelalberti.es>
2024-10-02 16:46:31 +0200

    commit 3

--- src/ej02.py
+++ src/ej02.py
@@ -2,9 +2,9 @@
 # Ejercicio 1.2.2
 # Escribe un programa para pedirle al usuario las horas de trabajo y el precio por hora y calcule el importe total del servicio.
 
-horas =int(input("¿Cuantas horas trabajas?\n"))
-Coste =int( input("¿Cuanto cobras por horas?\n"))
-Importe = horas*Coste
-print = ("horas de trabajo: "+ str(horas))
-print = ("coste de horas: "+ str(Coste))
-print = ("Importe total: "+ str(Importe))
+horas =int(input("Horas de trabajo: "))
+Coste =int( input("Coste por hora: "))
+
+Importe = horas * Coste
+
+print("Importe total: " + str(Importe))
