c4b264723c06438e2dbd1f88e1ffbe32b3db9662
bruninhogomez <gcbruno@iesrafaelalberti.es>
2024-10-04 19:25:01 +0200

    commit 6

--- src/numero_entero.py
+++ src/numero_entero.py
@@ -1,16 +1,18 @@
 
 def comprobar_entero(cadena : str) -> bool:
     cadena = cadena.strip()
-    cadena.isdigit() or (cadena.startswith("-") and cadena[1:].isdigit())
+    return cadena.isdigit() or (cadena.startswith("-") and cadena[1:].isdigit())
 
 def dame_entero() -> int: 
     cadena = input("Dame un entero: ")
     while not comprobar_entero(cadena):
         cadena = input("\n**ERROR** no es un entero!!!\n\nDame un entero de verdad:")
 
+    return int(cadena)
+
 def main():
-    num = dame_entero
-    print(f"Escribiste el número")
+    num = dame_entero()
+    print(f"Escribiste el número {num}")
 
 
 
