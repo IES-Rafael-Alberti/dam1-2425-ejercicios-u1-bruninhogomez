b2458a412bf80482d53fad32a288b9215a164a1f
bruninhogomez <gcbruno@iesrafaelalberti.es>
2024-10-15 21:29:28 +0200

    Commit 16 practica

--- src/ej17.py
+++ src/ej17.py
@@ -1,3 +1,11 @@
+"""
+Ej 17
+
+Este algoritmo imprime por pantalla el nombre del usuario las veces que el lo pida
+
+
+"""
+
 "Este es el ejercicio 17"
 #Escribir un programa que pregunte el nombre del usuario en la consola y un número entero e imprima 
 #por pantalla en líneas distintas el nombre del usuario tantas veces como el número introducido.
--- src/ej20.py
+++ src/ej20.py
@@ -3,6 +3,16 @@
 #código del país +34, y la extensión tiene dos dígitos (por ejemplo +34-913724710-56). Escribir un programa 
 #que pregunte por un número de teléfono con este formato y muestre por pantalla el número de teléfono sin el 
 #prefijo y la extensión.
+def pedir_num():
+    num = input("Introduce el numero de telefono: ")
+    return num
 
-numero = int(input("Introduce el numero de telefono con prefijo y extensión: "))
+def separar_num(num):
+    num.split("-")
 
+def main():
+    num = pedir_num()
+    num = separar_num(num)
+
+if __name__ == "__main__":
+    main()
