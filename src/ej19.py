03dbd3bd951301d056dce861f524e39c0eacb69d
bruninhogomez <gcbruno@iesrafaelalberti.es>
2024-10-15 17:18:37 +0200

    Commit 14

--- src/ej19.py
+++ src/ej19.py
@@ -2,3 +2,11 @@
 #Escribir un programa que pregunte el nombre del usuario en la consola y después de que el usuario lo
 #introduzca muestre por pantalla "NOMBRE tiene n letras.", donde NOMBRE es el nombre de usuario en mayúsculas 
 #y n es el número de letras que tienen el nombre.
+def main():
+
+    nombre = input("Introduce tu nombre completo: ")
+    n_letras = len(nombre)
+    print(f"{nombre.upper} tiene {n_letras} letras.")
+
+if __name__=="__main__":
+    main()
