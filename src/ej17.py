9c737d562af7f12483d36d4043177c7d579fce1e
bruninhogomez <gcbruno@iesrafaelalberti.es>
2024-10-15 16:30:59 +0200

    Commit 13

---
+++ src/ej17.py
@@ -0,0 +1,10 @@
+"Este es el ejercicio 17"
+#Escribir un programa que pregunte el nombre del usuario en la consola y un número entero e imprima 
+#por pantalla en líneas distintas el nombre del usuario tantas veces como el número introducido.
+
+nombre = input("Introduzca su nombre : ")
+
+numero = int(input("Introduce un numero entero : "))
+
+for _ in range(numero) :
+    print(nombre)
