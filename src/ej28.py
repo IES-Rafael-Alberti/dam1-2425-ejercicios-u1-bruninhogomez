5d040f7cb70bf2441076bf90a06b3d1414776d3c
bruninhogomez <gcbruno@iesrafaelalberti.es>
2024-10-22 15:44:52 +0200

    Commit20 he corregido el ej 28

--- src/ej28.py
+++ src/ej28.py
@@ -1,7 +1,6 @@
 "Este es el ejercicio 28"
 #Calcular el área de un triángulo a partir de tres lados
 
-import math
 
 def calcular_semiperimetro(a, b, c):
     s_op = a + b + c / 2
@@ -9,15 +8,18 @@ def calcular_semiperimetro(a, b, c):
 
 def calcular_area(a, b, c):
     s = calcular_semiperimetro(a, b, c)
-    area = math.sqrt(s * (s - a) * (s - b) * (s - c))
+    area = s * (s - a) * (s - b) * (s - c)
     return area
 
 def main():
     a = float(input("Introduce el lado a del triángulo: "))
     b = float(input("Introduce el lado b del triángulo: "))
     c = float(input("Introduce el lado c del triángulo: "))
+    
     if a + b > c or a + c > b or b + c > a:
-        area = calcular_area(a, b, c)
-        print(f"El área del triángulo es: {area:.2f}")
+        print(f"El área del triángulo es: {calcular_area(a, b, c):.2f}")
     else:
          print("Los lados proporcionados no forman un triángulo válido.")
+
+if __name__ == "__main__":
+    main()
